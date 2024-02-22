a8571ef0c5fcdd1644fe592fb571f2ab92fd7bad
AbdurRahman-34 <get.abdurahman@gmail.com>
2024-02-22 13:37:07 +0600

    Problem 1 git commit

---
+++ P1_How to remove false values from array/problem1.js
@@ -0,0 +1,19 @@
+// How to remove false values from array :::
+
+let = foodBox = ["Apple", null, undefined, "laychee", 0]
+let foodFilter = foodBox.filter(Boolean)
+console.log(foodFilter)
+
+// Output : [ 'Apple', 'laychee' ]
+
+
+
+
+/* Note : Js false values ::::
+➤ undefined , 
+➤ null , 
+➤ NaN , 
+➤ 0 , 
+➤ "" (empty string), 
+➤ false 
+*/
