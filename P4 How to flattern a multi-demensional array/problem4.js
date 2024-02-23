280d5843078d55911afc6d858c4acf089db66323
AbdurRahman-34 <get.abdurahman@gmail.com>
2024-02-23 10:54:08 +0600

    Problem4 solve git commit

---
+++ P4 How to flattern a multi-demensional array/problem4.js
@@ -0,0 +1,6 @@
+// How To Flattern a Multi-Demensinal Array :::
+const names = ["sadik", ["ayman", "sadman"], "salman", ["ibrahim", "hifzur"] ]
+console.log(names.flat())
+
+// multi lavel array :::
+console.log(names.flat(Infinity))
