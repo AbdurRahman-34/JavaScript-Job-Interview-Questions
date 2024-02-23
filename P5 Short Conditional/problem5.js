8eb67b723905b06c33b68c26e3c6619b75d09bb0
AbdurRahman-34 <get.abdurahman@gmail.com>
2024-02-23 11:04:50 +0600

    Problem5 solve git commit

---
+++ P5 Short Conditional/problem5.js
@@ -0,0 +1,13 @@
+const captain = "Sakib Khan";
+
+// if(captain === "Sakib Khan"){
+//     console.log("Winnner")
+// }
+
+
+// Short true Condtional : 1 
+captain === "Sakib Khan" && console.log("Winnners")
+
+
+// Short false Condtional : 2
+captain === "Sakib Khan" || console.log("Not Winner")
